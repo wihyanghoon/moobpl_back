36e3927df7f99f4073f7dc322a762a43a2b10333
wihyanghoon <gidgns1995@gmail.com>
2023-02-06 19:13:54 +0900

    session fix

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 5,
-        sameSite: 'strict',
+        sameSite: 'none',
         secure : true,
     }
 }));
